fa6c9e5810ad33e509a77bd5cebc0a8b40879c42
gabrielRaduDanila <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-09-02 20:32:27 +0000

    Deploying to gh-pages from @ gabrielRaduDanila/goit-react-hw-08-phonebook@065a0a66c61b590d2e713069774980f3a9ad2ec9 🚀

---
+++ static/js/172.f071ea84.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[172],{6172:function(r,e,a){a.r(e);var o=a(1965),t=a(184);e.default=function(){return(0,t.jsx)("div",{className:"loading-container",children:(0,t.jsx)(o.ko,{height:"120",width:"120",ariaLabel:"progress-bar-loading",wrapperStyle:{},wrapperClass:"progress-bar-wrapper",borderColor:"#F4442E",barColor:"#51E5FF"})})}}}]);
+//# sourceMappingURL=172.f071ea84.chunk.js.map
