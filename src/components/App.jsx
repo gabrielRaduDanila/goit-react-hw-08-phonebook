57141cc412c00a78f36681bf3ecf9ff1a2e519ed
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-09-04 09:05:33 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,29 +6,14 @@ import { refreshUser } from 'features/auth/operations';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useAuth } from 'hooks';
-<<<<<<< HEAD
 import SharedLayout from './shared-layout/SharedLayout';
-// import HomePage from './pages/home-page/HomePage';
-// import Register from './pages/register/Register';
-// import Login from './pages/login/Login';
-// import Contacts from './pages/contacts/Contacts';
-// import ErrorPage from './pages/error-page/ErrorPage';
 import Loading from './loading/Loading';
 
-=======
-import Loading from './loading/Loading'
->>>>>>> da9d52faf3093bae0ea960aed31f3804162faf53
 const HomePage = lazy(() => import('./pages/home-page/HomePage'));
 const Register = lazy(() => import('./pages/register/Register'));
 const Login = lazy(() => import('./pages/login/Login'));
 const Contacts = lazy(() => import('./pages/contacts/Contacts'));
-// const SharedLayout = lazy(() => import('./shared-layout/SharedLayout'));
 const ErrorPage = lazy(() => import('./pages/error-page/ErrorPage'));
-<<<<<<< HEAD
-// const Loading = lazy(() => import('./loading/Loading'));
-=======
-//const Loading = lazy(() => import('./loading/Loading'));
->>>>>>> da9d52faf3093bae0ea960aed31f3804162faf53
 
 export const App = () => {
   const { isRefreshing } = useAuth();
